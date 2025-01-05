f3e96b98c19249078080b69362c00cf34270acb7
percalissendorff <per.calissendorff@gmail.com>
2025-01-05 16:17:56 -0500

    Update mla_tablewords.py

--- mla_tablewords.py
+++ mla_tablewords.py
@@ -55,10 +55,22 @@ ename = exception_file.name
 st.write("Uploaded exception file: ", ename)
 if exception_file:
 	st.write("Uploaded exception file: ", ename)
-	#st.write("Successfully uploaded exception file")
 	#with open(exception_file,'r') as f:
-	exceps_raw = [line.strip() for line in exception_file]
-	exceps = [line.replace("b", "") for line in exceps_raw]
+	#exceps= [line.strip() for line in exception_file]
+if uploaded_file is not None:
+	# Read the file as bytes
+	file_contents = exception_file.read()
+
+	# If you need to handle the 'b' character:
+	if file_contents.startswith(b'\xef\xbb\xbf'):
+		file_contents = file_contents[3:]  # Remove BOM
+
+	# Decode the bytes to string (assuming UTF-8 encoding)
+	text = file_contents.decode("utf-8")
+
+	st.write("File contents:")
+	st.text(text)
+	exceps = text
 else:
 	with open('default_exceptions.dat','r') as f:
 		exceps = [line.strip() for line in f]
