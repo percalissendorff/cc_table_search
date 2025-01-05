a08018d340b2c58c37fe2fdc8f8d1f454c65abfb
percalissendorff <per.calissendorff@gmail.com>
2025-01-05 14:34:02 -0500

    Update mla_tablewords.py

--- mla_tablewords.py
+++ mla_tablewords.py
@@ -40,66 +40,7 @@ if uploaded_file is not None:
 
 ########################################################################
 
-# Collect all ID numbers and answers
-answers = read_functions.search_words(df)
 
-
-# Question IDs
-qids = list(answers.keys())
-
-# Make exceptions. Could import a list from separate docutment instead
-exception_file = st.file_uploader("Upload your exceptions here, should be .txt file containing a list for now.", type='txt', accept_multiple_files=False, key="up2")
-if uploaded_file is not None:
-	with open(exception_file, 'r') as f:
-		exceps = [line.strip() for line in f]
-else:
-	with open('default_exceptions.dat', 'r') as f:
-		exceps = [line.strip() for line in f]
-
-
-# Save flagged words and id
-flagged_word = []
-flagged_id = []
-flagged = {}
-
-# Step through all answers starting with first
-for i in range(0, len(qids)-1):
-	qid = qids[i]
-	split_answer = (" ".join(answers[qid])).split()
-	
-	for j in range(0, len(split_answer)):		# Step through words in answer
-		current_word = split_answer[j]
-		
-		for k in range(i+1, len(qids)):		# Step through other answers
-			# question ids
-			oqid = qids[k]		# other question ids
-			other_answers = (" ".join(answers[oqid])).split()
-			if current_word in other_answers:	# Find current word in other answers
-				# Ignore exceptions and shorter words
-				if current_word in exceps or len(current_word) < 3:
-					pass
-				else:							# Save multiple instances of word in answer
-					flagged_word.append(current_word)
-					flagged_id.append([qid, oqid])
-					
-					# Check if flagged word exists, otherwise create dictionary key
-					# and add matching word ID to key
-					if current_word not in flagged:
-						flagged.setdefault(current_word, []).append(qid)
-						flagged.setdefault(current_word, []).append(oqid)
-					# If word exists and is flagged, only add the following matching ID to key
-					else:
-						# Do not add the same ID several times for the same matching word
-						if oqid in flagged[current_word]:
-							pass
-						else:
-							flagged.setdefault(current_word, []).append(oqid)
-
-
-
-# Display the results
-for key, val in flagged.items():
-	st.write(key, ":", val)
 
 
 
