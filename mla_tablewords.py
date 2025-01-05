2707b994ed9ca7398ee36a45b027d42cf898f29f
percalissendorff <per.calissendorff@gmail.com>
2025-01-05 14:38:36 -0500

    Update mla_tablewords.py

--- mla_tablewords.py
+++ mla_tablewords.py
@@ -22,17 +22,18 @@ from streamlit import session_state
 #fname = '/home/per/Dropbox/Blandat/test_document_for_pancakedevil_onecol.odt'
 uploaded_file = st.file_uploader("Upload your table here, should be .odt file for now.", type='odt', accept_multiple_files=False, key="up1")
 
-st.write(uploaded_file.name)
+fname = uploaded_file.name
+st.write("Uploaded file: "+fname)
 
 if uploaded_file is not None:
 	# Read file
-	if ".docx" in uploaded_file:
+	if ".docx" in fname:
 		df = read_functions.read_docx(uploaded_file)
-	elif ".odt" in uploaded_file:
+	elif ".odt" in fname:
 		print("This is a print statemetn")
 		st.write("We made it this far")
 		df = pd.read_excel(uploaded_file, header=None)
-	elif ".pdf" in uploaded_file:
+	elif ".pdf" in fname:
 		df = read_functions.read_pdf(uploaded_file)
 	else:
 		print("Unknown data format for input data table. Try Word (.docx) or OpenOffice (.odt).")
